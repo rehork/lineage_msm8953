14d210026da9248f2e82783f93dee639a7f2eb39
rehork <67607997+rehork@users.noreply.github.com>
2023-10-28 17:36:56 +0200

    kernel_msm8953 - remove duplicated commit

--- drivers/mmc/host/sdhci.c
+++ drivers/mmc/host/sdhci.c
@@ -1390,10 +1390,6 @@ static u16 sdhci_get_preset_value(struct sdhci_host *host)
 	case MMC_TIMING_SD_HS:
 		preset = sdhci_readw(host, SDHCI_PRESET_FOR_HIGH_SPEED);
 		break;
-	case MMC_TIMING_MMC_HS:
-	case MMC_TIMING_SD_HS:
-		preset = sdhci_readw(host, SDHCI_PRESET_FOR_HIGH_SPEED);
-		break;
 	case MMC_TIMING_UHS_SDR12:
 		preset = sdhci_readw(host, SDHCI_PRESET_FOR_SDR12);
 		break;
